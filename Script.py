812ca26883d79bed342e1391c28fd4d43be0ec23
Mrbeanteddybot <103929536+Mrbeanteddybot@users.noreply.github.com>
2022-12-21 17:18:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,12 +4,13 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TechMagazineYT>TechMagazine</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: KOYEB
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+✯ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/TechMagazineYT>𝙈𝙍 𝘽𝙀𝘼𝙉</a>
+</a>
+✯ ʟɪʙʀᴀʀy: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+✯ ʟᴀɴɢᴜᴀɢᴇ: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✯ ᴅᴀᴛᴀ ʙᴀꜱᴇ: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+✯ ʙᴏᴛ ꜱᴇʀᴠᴇʀ: 𝙰𝙽𝚈𝚆𝙷𝙴𝚁𝙴
+✯ ʙᴜɪʟᴅ ꜱᴛᴀᴛᴜꜱ: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Elsa - AutoFilterBot is not a open source project. 
 
@@ -17,10 +18,10 @@ class script(object):
 - <a href=https://t.me/Hell_Botz>Hell Botz</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Elsa will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. TechMagazine-AutoFilterBot should have admin privillage.
+1. Elsa - AutoFilterBot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -31,15 +32,15 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- TechMagazine-AutoFilterBot Supports both url and alert inline buttons.
+- Elsa-AutoFilterBot Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. TechMagazine-AutoFilterBot supports buttons with any telegram media type.
+2. Elsa-AutoFilterBot supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/TechMagazineYT)</code>
+<code>[Button Text](buttonurl:https://t.me/Hell_Botz)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -66,7 +67,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of Elsa
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
